a9ac4746db8f35c5af13dbe5290a3ca6a9d9af2e
atakhan11 <atakhanwork@gmail.com>
2026-05-21 11:12:02 +0000

    6-sum_mixed_list.py

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that provides a function to sum a mixed list of integers and floats."""
+
+
+def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+    """Return the sum of a list containing integers and floats."""
+    return float(sum(mxd_lst))
